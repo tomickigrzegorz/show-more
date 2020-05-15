6fb5cfa3d6aae729d380585dd5e54bcd9377fc7b
Grzegorz Tomicki <gtomik@gmail.com>
2020-05-15 17:32:03 +0200

    eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "airbnb-base",
+        "prettier"
+    ],
+    "plugins": ["prettier"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "prettier/prettier": [
+            "error",
+            {
+                "endOfLine": "auto"
+            }
+        ],
+        "quotes": [2, "duble"],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "class-methods-use-this": "off",
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "linebreak-style": ["error", "windows"],
+        "no-param-reassign": [
+            2,
+            {
+                "props": false
+            }
+        ],
+        "no-plusplus": [
+            "error",
+            {
+                "allowForLoopAfterthoughts": true
+            }
+        ]
+    }
+};
