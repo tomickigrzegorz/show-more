6ebccfc5b6bc607df61d7a3277b3110516f53e5c
Grzegorz Tomicki <gtomik@gmail.com>
2021-01-30 13:06:35 +0100

    refactor: space 4 to 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-    },
-    extends: ['eslint:recommended'],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': [
-            'warn',
-            {
-                semi: true,
-                singleQuote: true,
-                tabWidth: 2,
-                endOfLine: 'auto',
-                printWidth: 80,
-            },
-        ],
-        'comma-dangle': ['error', 'only-multiline'],
-        'linebreak-style': ['error', 'windows'],
-        'no-param-reassign': [
-            2,
-            {
-                props: false,
-            },
-        ],
-    },
-    parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        endOfLine: 'auto',
+        printWidth: 80,
+      },
+    ],
+    'comma-dangle': ['error', 'only-multiline'],
+    'linebreak-style': ['error', 'windows'],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+  },
+  parser: '@babel/eslint-parser',
 };
