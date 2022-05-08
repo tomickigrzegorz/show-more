644b50fcb406e5be40d03a82daa14ab03ea327ec
Grzegorz Tomicki <gtomik@gmail.com>
2022-05-08 13:45:45 +0200

    feat: degault options

---
+++ sources/utils/defaults.js
@@ -0,0 +1,12 @@
+const defaultOptions = {
+  typeElement: "span",
+  more: false,
+  less: false,
+  number: false,
+  nobutton: false,
+  after: 0,
+  btnClass: "show-more-btn",
+  btnClassAppend: null,
+};
+
+export default defaultOptions;
